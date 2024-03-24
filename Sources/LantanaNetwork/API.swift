13b85cf7416901f27a0d04e7c0f9cda560f193da
Fito Toledano <fito.toledano@gmail.com>
2024-03-24 15:50:06 +0100

    add REST API protocol

---
+++ Sources/LantanaNetwork/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  
+//
+//  Created by Fito Toledano on 24/03/2024.
+//
+
+import Foundation
+
+protocol API {
+    var scheme: HTTPScheme { get }
+    var baseURL: String { get }
+    var path: String { get }
+    var parameters: [URLQueryItem] { get }
+    var method: HTTPMethod { get }
+}
