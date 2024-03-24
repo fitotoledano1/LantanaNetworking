392f4080468cb5a0a7143750b7fb52ef74f5dcbc
Fito Toledano <fito.toledano@gmail.com>
2024-03-24 15:50:24 +0100

    Add HTTPMethod

---
+++ Sources/LantanaNetwork/HTTPMethod.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPMethod.swift
+//  
+//
+//  Created by Fito Toledano on 24/03/2024.
+//
+
+import Foundation
+import SwiftUI
+
+enum HTTPMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
